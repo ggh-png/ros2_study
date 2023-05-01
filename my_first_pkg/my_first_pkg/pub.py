8744b90906b9599b7477210884828e5e2042ce17
ggh-png <0380089@gmail.com>
2023-05-02 00:11:28 +0900

    ros2 topic sub & pub

---
+++ my_first_pkg/my_first_pkg/pub.py
@@ -0,0 +1,29 @@
+import rclpy as rp
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+
+class TutlePub(Node):
+    def __init__(self):
+        super().__init__('turtle_pub')
+        self.publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        period = 0.5
+        self.timer = self.create_timer(period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 2.0
+        msg.angular.z = 2.0
+        self.publisher.publish(msg)
+
+
+def main(args=None):
+    rp.init(args = args)
+    turtle_pub = TutlePub()
+    rp.spin(turtle_pub)
+
+    turtle_pub.destroy_node()
+    rp.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ my_first_pkg/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'my_first_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ggh',
+    maintainer_email='0380089@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_first_node = my_first_pkg.my_first_node:main',
+            'sub = my_first_pkg.sub:main',
+            'pub = my_first_pkg.pub:main'
+        ],
+    },
+)
